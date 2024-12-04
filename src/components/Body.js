2a42808fa44b4a7d4f80f2adfdb8e25afe8329d2
vishal911m <vishal911m@gmail.com>
2024-12-04 16:13:41 +0530

    Search Bar Included

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,34 @@
+import { useState } from "react";
 import { restaurantList } from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
+const FilterData = (searchInput, restaurants) => {
+    const FilteredData = restaurants.filter((restaurant) => {
+        return restaurant.info.name.includes(searchInput);
+    })
+    return FilteredData;
+};
+
 const Body = () => {
+const [searchInput, setSearchInput] = useState("");
+const [restaurants, setRestaurants] = useState(restaurantList);
     return(
+        <>
+        <div className="search-bar">
+            <input 
+                className="search-input"
+                placeholder="Search"
+                value={searchInput}
+                onChange={(e)=> setSearchInput(e.target.value)}
+            />
+            <button
+                className="search-button"
+                onClick={() => {
+                    const data = FilterData(searchInput, restaurants);
+                    setRestaurants(data);                  
+                }}
+            >Search</button>
+        </div>
         <div className="body">
             {/* <RestaurantCard restaurant={restaurantList[0].info}/>
             <RestaurantCard restaurant={restaurantList[1].info}/>
@@ -13,11 +39,12 @@ const Body = () => {
             <RestaurantCard restaurant={restaurantList[6].info}/>
             <RestaurantCard restaurant={restaurantList[7].info}/> */}
             {
-            restaurantList.map((restaurant) => {
+            restaurants.map((restaurant) => {
                 return <RestaurantCard {...restaurant.info}/>;
             })
             }
         </div>
+        </>
     );
 };
 
